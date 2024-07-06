4cff6d784790b70f27f703d6f558110b462c40eb
Matthew M-B <matthew.mb@getjobber.com>
2024-07-06 19:58:50 -0400

    Add cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    }
+  site: 'https://matthewmacraebovell.com',
+  devToolbar: {
+    enabled: false, 
+  }
 });
